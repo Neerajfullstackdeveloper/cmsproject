2208391d2aa11eb54525f9b01697ed4131fde2b7
Neeraj <nppandey2003@gmail.com>
2025-12-12 17:20:38 +0530

    added email in google

--- src/components/EmailTemplatePanel.tsx
+++ src/components/EmailTemplatePanel.tsx
@@ -60,9 +60,9 @@ Address: Ground Floor, Property No.26/1, Ajay Enclave, New Ajanta Cinema, New De
     subject: 'Google Virtual Tool',
     body: `<p>Hi {{name}},</p>
 
-<p>We are pleased to confirm that your subscription with <strong>GlobalB2BMart.com</strong> has been successfully activated. As per our records, you have enrolled for the <strong>[Package Name as mentioned on the website]</strong>, effective from <strong>[Date of Joining]</strong>, with a tenure of <strong>[Tenure Period]</strong>. Your account has now been initiated in our system, and our onboarding team will begin setting up your company profile, uploading your product catalogue, and enabling all features included in your selected package to ensure maximum visibility and complete business support throughout your subscription period.</p>
+<p>We are pleased to confirm that your subscription with <strong>GlobalB2BMart.com</strong> has been successfully activated. As per our records, you have enrolled for the <strong>Google Virtual Tool</strong>, effective from <strong>01/01/2025</strong>, with a tenure of <strong>1yr</strong>. Your account has now been initiated in our system, and our onboarding team will begin setting up your company profile, uploading your product catalogue, and enabling all features included in your selected package to ensure maximum visibility and complete business support throughout your subscription period.</p>
 
-<p>For any assistance during your tenure, you may contact our support team at <strong>+91 [Support Number]</strong> or write to us at <strong>[Support Email ID]</strong>. We are committed to providing prompt and reliable service at all times.</p>
+<p>For any assistance during your tenure, you may contact our support team at <strong>011-41029790</strong> or write to us at <strong>webwavebusinesspvtltd@gmail.com </strong>. We are committed to providing prompt and reliable service at all times.</p>
 
 <p>For future payments, kindly ensure that all transactions are made only to the official company bank account, payment gateway, or UPI ID shared below. Any payment made to any other bank account, number, or UPI ID will not be considered valid, and GlobalB2BMart.com / Webwave Business Pvt. Ltd. will not be liable for such transactions. Our official payment details are as follows:</p>
 
